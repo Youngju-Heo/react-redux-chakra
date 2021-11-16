84b7db7e4807f2210394149f7b23bda6d1c8aa91
yjheo <youngju@innodep.com>
2021-11-17 08:34:25 +0900

    add base theme

--- src/base-theme.ts
+++ src/base-theme.ts
@@ -11,8 +11,18 @@ const baseTheme = extendTheme({
     global: (props: StyleFunctionProps | Record<string, unknown>) => ({
       body: {
         color: mode("gray.800", "whiteAlpha.900")(props),
-        bg: mode("white", "#3b3b3b")(props)
-      }
+        bg: mode("white", "#3b3b3b")(props),
+        transitionProperty: "background-color",
+        transitionDuration: "normal",
+        lineHeight: "base",
+      },
+      "*::placeholder": {
+        color: mode("gray.400", "whiteAlpha.400")(props),
+      },
+      "*, *::before, &::after": {
+        borderColor: mode("gray.200", "whiteAlpha.300")(props),
+        wordWrap: "break-word",
+      },
     }),
   },
 });
