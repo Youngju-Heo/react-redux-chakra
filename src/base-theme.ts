d80b8e14ad55bcd48082ad94ec96e5f295afa77c
yjheo <youngju@innodep.com>
2021-11-16 23:41:01 +0900

    add theme

--- src/base-theme.ts
+++ src/base-theme.ts
@@ -1,9 +1,19 @@
+/* eslint-disable */
 import { extendTheme } from "@chakra-ui/react";
+import { mode, StyleFunctionProps } from "@chakra-ui/theme-tools";
 
 const baseTheme = extendTheme({
   fonts: {
     heading: "Noto Sans KR",
     body: "Noto Sans KR",
+  },
+  styles: {
+    global: (props: StyleFunctionProps | Record<string, unknown>) => ({
+      body: {
+        color: mode("gray.800", "whiteAlpha.900")(props),
+        bg: mode("white", "#3b3b3b")(props)
+      }
+    }),
   },
 });
 
