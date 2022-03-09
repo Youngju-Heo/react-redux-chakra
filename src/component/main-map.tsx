52b1e4e2cc8ee068abf1d52d1798e2c2e15ba1ce
yjheo <youngju@innodep.com>
2022-03-09 23:07:52 +0900

    그리기 샘플 추가.

--- src/component/main-map.tsx
+++ src/component/main-map.tsx
@@ -1,13 +1,22 @@
 /* eslint-disable no-console */
 import React from "react";
-import { RMap, RControl } from "rlayers";
+import { RMap, RControl, RInteraction, RLayerVector, RStyle } from "rlayers";
 import { RView } from "rlayers/RMap";
 import { MapProjection } from "./geoutil/map-projection";
 import { fromLonLat, toLonLat } from "ol/proj";
 import LayerBaroMap from "./geoutil/layer-baro-map";
 import { MapBrowserEvent, Map } from "ol";
-
 import "./map-main.scss";
+// import VectorSource from "ol/source/Vector";
+// import { Geometry } from "ol/geom";
+import {
+  altShiftKeysOnly,
+  platformModifierKeyOnly,
+  shiftKeyOnly,
+  altKeyOnly,
+  never,
+  doubleClick,
+} from "ol/events/condition";
 
 export interface MainMapProps {
   view: RView;
@@ -15,6 +24,7 @@ export interface MainMapProps {
 
 export const MainMap = (props: MainMapProps): JSX.Element => {
   const [view, setView] = React.useState<RView>({ center: fromLonLat(props.view.center, "EPSG:5179"), zoom: 15 });
+  // const [selected, setSelected] = React.useState(false);
 
   return (
     <RMap
@@ -36,6 +46,31 @@ export const MainMap = (props: MainMapProps): JSX.Element => {
     >
       <RControl.RScaleLine />
       <LayerBaroMap sourcePath={"https://tms-gis-tile.azurewebsites.net/api/v1/tile/{0}/{1}/{2}"} />
+
+      <RLayerVector
+        onChange={React.useCallback((e) => {
+          console.log(e);
+          // On every change, check if there is a feature covering the Eiffel Tower
+          // const source = e.target as VectorSource<Geometry>;
+          // if (source?.forEachFeatureAtCoordinateDirect)
+          //   setSelected(source.forEachFeatureAtCoordinateDirect(TourEiffel, () => true));
+        }, [])}
+      >
+        {/* This is the style used for the drawn polygons */}
+        <RStyle.RStyle>
+          <RStyle.RStroke color="#0000ff" width={3} />
+          <RStyle.RFill color="rgba(0, 0, 0, 0.75)" />
+        </RStyle.RStyle>
+
+        <RInteraction.RDraw type={"LineString"} condition={shiftKeyOnly} freehandCondition={altShiftKeysOnly} />
+
+        <RInteraction.RDraw type={"Circle"} condition={altKeyOnly} freehandCondition={never} />
+
+        <RInteraction.RModify
+          condition={platformModifierKeyOnly}
+          deleteCondition={React.useCallback((e) => platformModifierKeyOnly(e) && doubleClick(e), [])}
+        />
+      </RLayerVector>
     </RMap>
   );
 };
